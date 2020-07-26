4fdb04f46520bc2124aa8a362334a7e12b6eaf63
BinThani <62651802+BinThani@users.noreply.github.com>
2020-07-26 07:48:53 +0400

    Create squares.py

---
+++ squares.py
@@ -0,0 +1,10 @@
+squares = [] # Initialization
+
+for value in range(1, 11):
+    square = value**2  # 1 to 10 Squares them and, puts them in the Variable square
+    squares.append(square)  # appends values of square into the list squares.
+
+print(squares)
+
+# Output
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
