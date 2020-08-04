b3e33e377e30c96de0015e6f61c32fb9ca2e952e
BinThani <62651802+BinThani@users.noreply.github.com>
2020-08-04 10:45:33 +0400

    Update recursion.py

--- PythonTests/Recursion/recursion.py
+++ PythonTests/Recursion/recursion.py
@@ -17,6 +17,17 @@ def factorial(n):
         return 1
     else:
         return n*factorial(n-1)  # Recursive Case
+    
+def fib(x):
+    """
+    assumes x an int >= 0
+    returns Fibonacci of x
+    """
+    if x == 0 or x == 1: # Multiple Base cases
+        return 1
+    else:
+        return fib(x-1) + fib(x-2) # Multiple recursive cases
 
 if __name__ == '__main__':
     print(factorial(4))
+    print(fib(4))
